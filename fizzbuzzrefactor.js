25f17f4d1dde0f48b6243c60e5fc0429eb2ae5ff
jollypeanut <sking@crazyawesome.io>
2015-11-12 10:30:26 -0600

    Initial commit of fizzbuzz refactor js.

---
+++ fizzbuzzrefactor.js
@@ -0,0 +1,17 @@
+// FizzBuzz Refactor Program
+// Skylar King
+// Prints 1-100, if divisible by 3 prints fizz, if divisible by 5 prints buzz, if divisible by both prints buzz.
+
+for (num = 1; num <=100; num++) {
+  if (num % 15 === 0)
+    console.log("FizzBuzz");
+
+  else if (num % 3 === 0)
+    console.log("Fizz");
+
+  else if (num % 5 === 0)
+    console.log("Buzz");
+
+  else
+    console.log(num);
+}
