2350ad164f1995a2c9715fdc08ed7451a1f6fc9f
jollypeanut <sking@crazyawesome.io>
2015-11-12 12:03:18 -0600

    Complete refactor, app is confirmed to work.

--- fizzbuzzrefactor.js
+++ fizzbuzzrefactor.js
@@ -1,29 +1,27 @@
 // FizzBuzz Refactor Program
 // Skylar King
-// Prints 1-100, if divisible by 3 prints fizz, if divisible by 5 prints buzz, if divisible by both prints buzz.
+// Prompts user for ending number. Starting number defaults to '1'.
+// If numbers in range are divisible by 3 prints fizz, if divisible by 5 prints buzz,
+// if divisible by both prints fizzbuzz.
 
-function numberEntry () {
+function startFizzBuzz () {
   startingNum = 1
   endingNum = prompt("Please type a number to start FizzBuzz.");
   endingNumConversion = parseInt(endingNum);
-  numberValidation()
+  isNumberValidation();
 }
 
-function numberValidation () {
+function isNumberValidation () {
   if (isNaN (endingNumConversion) === true) {
       alert("That is not a number. Please try again.");
-      numberEntry();
+      startFizzBuzz();
   }
   else {
-    console.log("It works so far!");
+    FizzBuzz();
   }
 }
 
-function fizzbuzz (startingNum , endingNum) {
-  var startingNum = 1
-  var endingNum = prompt("Please type a number to start FizzBuzz.");
-  var endingNumConversion = parseInt(endingNum);
-
+function FizzBuzz () {
   for (num = startingNum; num <= endingNumConversion; num++) {
     if (num % 15 === 0)
       console.log("FizzBuzz");
