98bab9987e36cc0bc669a2e0a923cb324bf97d52
jollypeanut <sking@crazyawesome.io>
2015-11-12 10:43:34 -0600

    Added function to take starting default of 1 and ending value from user input. FizzBuzzes numbers until ending value is reached.

--- fizzbuzzrefactor.js
+++ fizzbuzzrefactor.js
@@ -2,18 +2,21 @@
 // Skylar King
 // Prints 1-100, if divisible by 3 prints fizz, if divisible by 5 prints buzz, if divisible by both prints buzz.
 
-var startingNum = prompt("Please type a number to start FizzBuzz.");
+function fizzbuzz (startingNum , endingNum) {
+  var startingNum = 1
+  var endingNum = prompt("Please type a number to start FizzBuzz.");
 
-for (num = 1; num <=100; num++) {
-  if (num % 15 === 0)
-    console.log("FizzBuzz");
+  for (num = startingNum; num <= endingNum; num++) {
+    if (num % 15 === 0)
+      console.log("FizzBuzz");
 
-  else if (num % 3 === 0)
-    console.log("Fizz");
+    else if (num % 3 === 0)
+      console.log("Fizz");
 
-  else if (num % 5 === 0)
-    console.log("Buzz");
+    else if (num % 5 === 0)
+      console.log("Buzz");
 
-  else
-    console.log(num);
+    else
+      console.log(num);
+  }
 }
