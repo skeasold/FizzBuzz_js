79df9b7af1a329966bc576a3ac355bf0086276f9
jollypeanut <sking@crazyawesome.io>
2015-11-12 11:20:13 -0600

    Added number entry and number validation functions. Set variables within number entry function to global variables.

--- fizzbuzzrefactor.js
+++ fizzbuzzrefactor.js
@@ -2,11 +2,29 @@
 // Skylar King
 // Prints 1-100, if divisible by 3 prints fizz, if divisible by 5 prints buzz, if divisible by both prints buzz.
 
+function numberEntry () {
+  startingNum = 1
+  endingNum = prompt("Please type a number to start FizzBuzz.");
+  endingNumConversion = parseInt(endingNum);
+  numberValidation()
+}
+
+function numberValidation () {
+  if (isNaN (endingNumConversion) === true) {
+      alert("That is not a number. Please try again.");
+      numberEntry();
+  }
+  else {
+    console.log("It works so far!");
+  }
+}
+
 function fizzbuzz (startingNum , endingNum) {
   var startingNum = 1
-  var endingNum = +prompt("Please type a number to start FizzBuzz.");
+  var endingNum = prompt("Please type a number to start FizzBuzz.");
+  var endingNumConversion = parseInt(endingNum);
 
-  for (num = startingNum; num <= endingNum; num++) {
+  for (num = startingNum; num <= endingNumConversion; num++) {
     if (num % 15 === 0)
       console.log("FizzBuzz");
 
