f16851f1c298bd5d18aa4093b9a41eec8c6f4f43
jollypeanut <sking@crazyawesome.io>
2015-11-12 10:34:25 -0600

    Prompt to supply a number.

--- fizzbuzzrefactor.js
+++ fizzbuzzrefactor.js
@@ -2,6 +2,8 @@
 // Skylar King
 // Prints 1-100, if divisible by 3 prints fizz, if divisible by 5 prints buzz, if divisible by both prints buzz.
 
+var startingNum = prompt("Please type a number to start FizzBuzz.");
+
 for (num = 1; num <=100; num++) {
   if (num % 15 === 0)
     console.log("FizzBuzz");
